d8c8211a5f2f784309b87b59c433294d69569edc
lanberb <lanberb.2@gmail.com>
2024-01-03 11:39:57 +0900

    refactor: contactとsponcerの修正

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,12 +1,18 @@
+import clsx from "clsx";
 import { CONTACT_X_DESCRIPTION, CONTACT_X_EMOJI } from "~/libs/dictionary";
 
 export const ContactX = () => {
   return (
-    <div className="bg-gradient-primary relative mx-auto w-full max-w-601 space-y-8 rounded-24 px-12 pb-24 pt-12 text-center before:absolute before:inset-0 before:m-6 before:rounded-24 before:bg-white child:relative md:px-80 md:pb-36 md:pt-24">
-      <div className="text-40 leading-100">{CONTACT_X_EMOJI}</div>
-      <div className="text-14 font-bold leading-[200%] text-navy md:text-20">
+    <div
+      className={clsx(
+        "bg-gradient-primary relative mx-auto flex w-full flex-col items-center space-y-8 rounded-24 px-0 pb-24 pt-12 md:w-fit md:px-80 md:pb-36 md:pt-36",
+        "before:absolute before:inset-0 before:m-6 before:rounded-20 before:bg-white child:relative ",
+      )}
+    >
+      <h2 className="w-fit text-40 leading-100">{CONTACT_X_EMOJI}</h2>
+      <div className="text-center text-14 font-bold leading-140 tracking-[1.2px] text-navy md:text-20 md:tracking-2">
         {CONTACT_X_DESCRIPTION.map((desc, index) => (
-          <div key={index} dangerouslySetInnerHTML={{ __html: desc }} />
+          <p key={index} dangerouslySetInnerHTML={{ __html: desc }} />
         ))}
       </div>
     </div>
--- src/renderer/+onRenderHtml.tsx
+++ src/renderer/+onRenderHtml.tsx
@@ -38,7 +38,7 @@ const onRenderHtml: OnRenderHtmlAsync = async (
         <link rel="icon" href="/favicon.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Noto+Sans+JP:wght@300;500;800&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Noto+Sans+JP:wght@300;600;800&display=swap" rel="stylesheet">
         <meta property="og:type" content="website" />
         <meta property="og:url" content="${WEBSITE_URL}" />
         <meta property="og:locale" content="ja_JP" />
